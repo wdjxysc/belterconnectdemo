58ee61a46a50f102b6fe2914507675d6c2a38477
wdjxysc <wdjxysc@163.com>
2014-08-09 11:47:47 +0800

    postjson
    postjson

---
+++ belterconnectdemoTests/belterconnectdemoTests.m
@@ -0,0 +1,34 @@
+//
+//  belterconnectdemoTests.m
+//  belterconnectdemoTests
+//
+//  Created by 夏 伟 on 14-8-8.
+//  Copyright (c) 2014年 夏 伟. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface belterconnectdemoTests : XCTestCase
+
+@end
+
+@implementation belterconnectdemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
